243da58e581d2cb876255a9135e344d48aa2eaa3
smsarkar1994 <60992047+smsarkar1994@users.noreply.github.com>
2022-08-25 11:57:01 -0400

    Add files via upload

--- ui.R
+++ ui.R
@@ -13,11 +13,11 @@ ui <- fluidPage(
   
   # App title ----
   titlePanel(
-    h1("Dynamic BeerSheet"), windowTitle = "Dynamic-Beersheet"
+    h1("Dynamic BeerSheet", style = "font-size:125%;"), windowTitle = "Dynamic-Beersheet"
   ),
   
   titlePanel(
-    h5("Author: Sameer Sarkar")
+    h5("Author: Sameer Sarkar", style = "font-size:125%;")
   ),
   
   # Sidebar layout with input and output definitions ----
@@ -31,12 +31,12 @@ ui <- fluidPage(
                  # textOutput("test"),
                  # br(),
                  # br(),
-                 h4("Download Custom BeerSheet Template"),
-                 p(a(href = "https://www.dropbox.com/s/kqp0y4nmtqcbzel/custom_beersheet_template.xlsx?dl=0",
+                 h4("Download Custom BeerSheet Template", style = "font-size:125%;"),
+                 p(style = "font-size:125%;", a(href = "https://www.dropbox.com/s/kqp0y4nmtqcbzel/custom_beersheet_template.xlsx?dl=0",
                      'Click here for Dropbox download link.',
-                     target="_blank")),
+                     target="_blank"), style = "font-size:125%;"),
                  br(),
-                 h4("Upload File"),
+                 h4("Upload File", style = "font-size:125%;"),
                  fileInput("file1", "Choose Custom BeerSheet File (see instructions)",
                            multiple = TRUE),
                  br(),
@@ -54,40 +54,40 @@ ui <- fluidPage(
     mainPanel(position = "right",
               tabsetPanel(
                 tabPanel("Description",
-                         h3("Description"),
-                         p("Welcome to my Dynamic BeerSheet App. Drafting in 
+                         h3("Description", style = "font-size:125%;"),
+                         p(style = "font-size:125%;", "Welcome to my Dynamic BeerSheet App. Drafting in 
                          Fantasy is a frantic and stressful process. Luckily, there are 
                          lots of great draft kits out there to help us out -
                          in particular the fantastic BeerSheets created by Reddit
-                         user /u/BeerSheets."),
-                         p("However, it can be tough to simultaneously juggle
+                         user /u/BeerSheets.", style = "font-size:125%;"),
+                         p(style = "font-size:125%;", "However, it can be tough to simultaneously juggle
                            the stress of the draft and update your BeerSheets
                            as players are being taken off the board. This is especially
                            hard if you have tight draft windows (<30s) or do in-person
                            drafts where you can be distracted by the antics of your leaguemates"),
-                         p("Enter my", strong("Dynamic BeerSheets App."), "This app is designed to 
+                         p(style = "font-size:125%;", "Enter my", strong("Dynamic BeerSheets App."), "This app is designed to 
                            integrate with your draft and", strong("dynamically update your BeerSheet 
                                                                    to show who's been drafted and who's still left.")),
-                         p("Note that this app currently
+                         p(style = "font-size:125%;", "Note that this app currently
                          only works for", strong("Sleeper Leagues."), "If there is 
                          sufficient interest, I will look into generalizing this app for 
                            ESPN and Yahoo Leagues."),
-                         p("This app requires the following:"),
-                         p("1) A Google Account (the dynamic spreadsheet will be a 
+                         p(style = "font-size:125%;", "This app requires the following:"),
+                         p(style = "font-size:125%;", "1) A Google Account (the dynamic spreadsheet will be a 
                            Google Sheet)."),
-                         p("2) Microsoft Excel."),
+                         p(style = "font-size:125%;", "2) Microsoft Excel."),
                          br(), 
-                         p(strong("P.S."), "If you like this app, feel free to check out", 
+                         p(style = "font-size:125%;", strong("P.S."), "If you like this app, feel free to check out", 
                            a(href = "https://www.statswithsasa.com", 
                              'my blog', target="_blank"), "for other cool things. 
                            I write about everything ranging from sports to current events."),
-                         p(strong("P.P.S."), "I'm hosting this app using a service that provides limited
+                         p(style = "font-size:125%;", strong("P.P.S."), "I'm hosting this app using a service that provides limited
                            free bandwidth. If you liked using this app, please consider",
                            a(href = "https://www.buymeacoffee.com/statswithsasa", 
-                             "throwing me a few bucks"),
+                             "throwing me a few bucks", target="_blank"),
                            "to help with server costs."),
                          br(),
-                         p(strong("P", tags$sup(3), ".S."),"Shoutout to the great folk(s) at BeerSheets and the awesome
+                         p(style = "font-size:125%;", strong("P", tags$sup(style = "font-size:125%;", 3), ".S."),"Shoutout to the great folk(s) at BeerSheets and the awesome
                            work they do. As well as Sleeper for having a well-documented
                            and easy-to-use API.")
                          
@@ -96,34 +96,39 @@ ui <- fluidPage(
                 
                 tabPanel("Instructions",
                          h3("Instructions"),
-                         p("To run this app, follow these instructions:"),
-                         p("1) Download the", strong("Custom BeerSheets Template"), "from the 
+                         p(style = "font-size:125%;", "To run this app, follow these instructions:"),
+                         p(style = "font-size:125%;", "1) Download the", strong("Custom BeerSheets Template"), "from the 
                            panel on the left."),
-                         p("2) Go to the ", a(href = "https://footballabsurdity.com/beersheet-request-form/", "BeerSheets site"),
+                         p(style = "font-size:125%;", "2) Go to the ", a(href = "https://footballabsurdity.com/beersheet-request-form/", "BeerSheets site", target="_blank"),
                            "and request a BeerSheet based on your league settings."), 
-                         p("3) Download ", strong("BOTH"), "the .csv and .xlsx files from BeerSheets."),
-                         p('4) Paste the .csv file into the "BeerSheet_csv_raw" tab and the .xlsx file 
+                         p(style = "font-size:125%;", "3) Download ", strong("BOTH"), "the .csv and .xlsx files from BeerSheets."),
+                         p(style = "font-size:125%;", '4) Paste the .csv file into the "BeerSheet_csv_raw" tab and the .xlsx file 
                            into the "Beersheet_xlsx_raw" tab of the template. Take care to', strong("completely overwrite"),
                            "anything in those tabs. I recommend copy and pasting the entire sheet from the source."),
-                         p("5) Navigate to", a(href = "https://drive.google.com/drive/my-drive", "Google Drive"),
+                         p(style = "font-size:125%;", "5) Navigate to", a(href = "https://drive.google.com/drive/my-drive", "Google Drive", target="_blank"),
                            "and create a new Google Sheet."),
-                         p("6) In the new Google Sheet, upload the Formatted Custom BeerSheet by 
+                         p(style = "font-size:125%;", "6) In the new Google Sheet, upload the Formatted Custom BeerSheet by 
                            navigating to File -> Import -> Upload and selecting the formatted custom excel file."),
-                         p("7) Edit the Google Sheet permissions so anyone can edit it. You can do so by clicking the green \"Share\"
+                         p(style = "font-size:125%;", "7) Edit the Google Sheet permissions so anyone can edit it. You can do so by clicking the green \"Share\"
                            button on the top right -> click \"save\" -> under \"General Access\", 
                            change it so \"Anyone with the link\" is an editor", strong("(not just a viewer)."),
                            "The app WILL NOT work if you don't do this step."),
-                         p('8) In the "metadata" tab of the', strong("Excel file (not the Google Sheet)"),
+                         p(style = "font-size:125%;", '8) In the "metadata" tab of the', strong("Excel file (not the Google Sheet)"),
                            "update the 3 fields with a link to your Sleeper draft room, your Sleeper usename, 
                            and a link to the Google Sheet you just created."),
-                         p("9) Upload the Formatted Custom BeerSheet using the side panel on the left."),
-                         p("10) You're ready to go! Click the \"Update BeerSheet\"
+                         p(style = "font-size:125%;", "9) Upload the Formatted Custom BeerSheet using the side panel on the left."),
+                         p(style = "font-size:125%;", "10) You're ready to go! Click the \"Update BeerSheet\"
                            button whenever you want to update the Google Sheet
-                           with your draft progress."),
+                           with your draft progress. If the app has successfully executed,
+                           some information about your draft and when the Dynamic BeerSheet
+                           was last updated will display on the sidepanel."),
                          br(),
-                         p(strong("Note:"), "If your screen greys out at any time, 
-                           it means the app has crashed. Refresh it and make sure you've
-                           followed every step of these instructions.")
+                         p(style = "font-size:125%;", strong("Note:"), "If your screen greys out at any time, 
+                           it means the app has crashed or your session has timed out. 
+                           Refresh it and make sure you've followed every step of these instructions. 
+                           If the session has just timed out, all you need to do is refresh and reupload
+                           the Formatted Custom BeerSheet on the left to keep using the app. Nothing further
+                           required to keep using the app after the initial setup.")
                          
                 )
               )
